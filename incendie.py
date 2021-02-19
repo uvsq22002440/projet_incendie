d00badeecc0300288ad1c975af4c357f322f5b62
Anas SHAIK <anas.shaik@ens.uvsq.fr>
2021-02-19 12:17:34 +0100

    creation du quadrillage

--- incendie.py
+++ incendie.py
@@ -26,7 +26,7 @@ LARGEUR=600
 HAUTEUR=400
 COULEUR_FOND ="white"
 COTE_PARCELLE = 25
-COULEUR_LIGNE ="black"
+COULEUR_LIGNE ="grey60"
 COULEUR_PRAIRIE ="ligthgreen"
 COULEUR_FORET ="darkgreen"
 COULEUR_FEU ="red"
@@ -59,7 +59,16 @@ COULEUR_EAU ="cyan"
 #########################################################
 #définition des fonctions 
 
-
+def quadrillage():
+    """Affiche un quadrillage constitué de carrés de côté COTE_PARCELLE"""
+    y = 0
+    while y < HAUTEUR:
+        canvas.create_line((0, y), (LARGEUR, y), fill=COULEUR_LIGNE)
+        y += COTE_PARCELLE
+    x = 0
+    while x < LARGEUR:
+        canvas.create_line((x, 0), (x, HAUTEUR), fill=COULEUR_LIGNE)
+        x += COTE_PARCELLE
 
 
 
@@ -75,7 +84,7 @@ racine = tk.Tk()
 canvas = tk.Canvas(racine,width=LARGEUR,height=HAUTEUR,bg=COULEUR_FOND)
 canvas.grid()
 
-
+quadrillage()
 racine.mainloop()
 #########################################################
 
